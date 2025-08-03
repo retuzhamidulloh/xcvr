050b49f7896ebf451b60e856527c09baf752c3f4
retuzhamidulloh <retuz416@gmail.com>
2025-08-03 14:19:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let name = prompt ("Ismigizni kiritig")
+console.log = (name) ;
+
+let familiya = prompt ("Familiyagizni kiriting")
+console.log = (name)
+
+let yosh = prompt ("Yoshingizni kiriting")
+console.log = (name)
+
+let fan = prompt ("Yoqtirgan fanigizni kiriting")
+console.log = (name)
+
+let yashaydigan = prompt ("Yashaydigan tumanigizni kiriting")
+console.log = (name)
+
+let malumat = alert ("Malumotigizni kiritganigiz uchun tashakur")
+console.log = (name)
